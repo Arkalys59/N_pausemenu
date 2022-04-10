689923292e58e126d7bdc3290a9fa7461da7b03d
NoxZi-_- <96413659+NoxZi73@users.noreply.github.com>
2022-04-10 12:53:01 +0200

    Create n_pausemenu.lua

---
+++ n_pausemenu.lua
@@ -0,0 +1,13 @@
+Citizen.CreateThread(function()
+  AddTextEntry('FE_THDR_GTAO', '~b~TNDev ~^9~- ~^9~ID : ' .. GetPlayerServerId(PlayerId()) .. '~u~ - ~b~https://discord.gg/vJzqtd3vqR ~u~')
+  AddTextEntry('PM_PANE_LEAVE', '~^9~Retourner sur la liste des serveurs.')
+  AddTextEntry('PM_PANE_QUIT', '~b~Quitter TNDev')
+  AddTextEntry('PM_SCR_MAP', 'Carte de TNDev')---- a la place de carte tn le nom de votre serveur : Carte nom de votre serv
+  AddTextEntry('PM_SCR_GAM', '~^9~Prendre l\'avion')
+  AddTextEntry('PM_SCR_INF', '~b~Logs')
+  AddTextEntry('PM_SCR_SET', '~^9~Configuration')
+  AddTextEntry('PM_SCR_STA', '~b~Statistiques')
+  AddTextEntry('PM_SCR_RPL', '~^9~Éditeur ∑')
+end)
+
+-------Scripts by NoxZi-_-
